3b2346ccc0cedd345325e647e3343bdb9b4e05ee
M1Stick <sauronchik26@gmail.com>
2020-06-24 17:23:23 +0300

    slider complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,19 @@
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: Infinity,
+        autoplaySpeed: 2500,
+        adaptiveHeight: true
+      });
   });
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'), 
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+          
