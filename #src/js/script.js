fb2755f1386634c64ab107b139d8afe44197cd2c
M1Stick <sauronchik26@gmail.com>
2020-06-25 04:50:08 +0300

    adaptive complete (need small fixes with burger and post elements)

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,7 +5,21 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: Infinity,
         autoplaySpeed: 2500,
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        responsive: [
+	    {
+	      breakpoint: 769,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+    ]
       });
   });
 $(document).ready(function(){
